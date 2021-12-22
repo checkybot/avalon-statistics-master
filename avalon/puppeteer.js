0e831d8899cbae80a8190e2576c76f48b78f1008
checkybot <checkybot@yandex.ru>
2021-12-22 23:02:07 +0200

    init

---
+++ avalon/puppeteer.js
@@ -0,0 +1,236 @@
+'use strict';
+
+require('dotenv').config();
+
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const Discord = require('discord.js');
+const data = require('../data/data5');
+// const gamePages = require('../links');
+
+// const discordHook = new Discord.WebhookClient(process.env.API_ID, process.env.API_TOKEN);
+
+const emailInputId = '#username';
+const passwordInputId = '#password';
+const loginButtonId = '.el-button.login__button.el-button--default';
+const login = process.env.LOGIN;
+const password = process.env.PASSWORD;
+const lobbySelector = '.lobby';
+const nameSelector = '.player__username';
+const loginPage = 'https://avalon.fun/login';
+let page;
+const messages = new Set();
+const messagesOld = new Set(Object.keys(data));
+const dates2 = {};
+
+const getGameData = () => {
+    const results = {
+        names: undefined,
+        roles: undefined,
+        redWin: false,
+        blueWin: false,
+        merlinShot: undefined,
+        byMissions: undefined
+    };
+    const roles = $('.player-avatar.player__player-avatar > .player-avatar__container > .player-avatar__avatar')
+        .map(( __, role ) => $(role).css('background-image').match(/img\/(\w*)/)[1])
+        .get();
+
+    if ( roles.length === 0 ) {
+        return;
+    }
+
+    const names = $('.player__username')
+        .text()
+        .replace(/\n\s*/g, ',')
+        .substring(1)
+        .slice(0, -1)
+        .split(',')
+        .map(name => name.replace(/[^\w\d\sа-яА-Я]/g, ''));
+
+    if ( names.length <= 6 ) {
+        return;
+    }
+
+    const successCount = $('.g-tbl__quests > .quest--success').length;
+    const failCount = $('.g-tbl__quests > .quest--fail').length;
+
+    const shotRoleIndex = $('.player__x-mark').map(( i, e ) => {
+        if ( $(e).css('opacity') === '1' ) {
+            return i;
+        }
+    }).get()[0];
+
+    if ( typeof shotRoleIndex !== 'undefined' && roles[shotRoleIndex] === 'merlin' ) {
+        results.merlinShot = true;
+    }
+    if ( successCount === 3 && !results.merlinShot ) {
+        results.blueWin = true;
+        results.byMissions = true;
+    } else if ( failCount === 3 ) {
+        results.redWin = true;
+        results.byMissions = true;
+        results.merlinShot = false;
+    } else {
+        results.redWin = true;
+        results.byMissions = false;
+    }
+    results.names = names;
+    results.roles = roles;
+
+    return new Promise(resolve => {
+        resolve(results);
+    });
+};
+
+const authorization = async () => {
+    await page.goto(loginPage, {waitUntil: 'networkidle2'});
+    await page.waitForSelector(emailInputId);
+    await page.type(emailInputId, login);
+    await page.type(passwordInputId, password);
+    await page.click(loginButtonId);
+    await page.click(loginButtonId);
+    await page.waitForSelector(lobbySelector);
+};
+
+const loadPage = async gameLink => {
+    await page.goto(gameLink, {waitUntil: 'networkidle2'});
+
+    try {
+        await page.waitForSelector(nameSelector, {
+            timeout: 500
+        });
+        await page.evaluate(async () => {
+            const script = await document.createElement('script');
+
+            script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+            await document.getElementsByTagName('head')[0].appendChild(script);
+        });
+        await page.waitForTimeout(500);
+    } catch ( e ) {
+        return;
+    }
+
+    return await page.evaluate(getGameData);
+};
+
+const loadMessages = async ( channel, lastMessageId ) => {
+    const messageCount = Number.MAX_SAFE_INTEGER;
+    // const messageCount = 5;
+    const fetchOptions = {limit: 100};
+    let fetchComplete = false;
+    let i = 1;
+
+    while ( !fetchComplete ) {
+        if ( lastMessageId ) {
+            fetchOptions.before = lastMessageId;
+        }
+
+        const fetched = await channel.messages.fetch(fetchOptions);
+
+        console.log('i = ', (i * 100));
+        i++;
+        console.log(messages.size);
+        if ( fetched.size === 0 ) {
+            break;
+        }
+        lastMessageId = fetched.last().id;
+        await Promise.all(fetched.map(async msg => {
+            if ( messages.size >= messageCount ) {
+                fetchComplete = true;
+
+                return;
+            }
+            if ( msg.cleanContent.includes('https://avalon.fun/GAME-') ) {
+                const link = msg.cleanContent.match(/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,?^=%&:\/~+#-]*[\w?^=%&\/~+#-])/)[0];
+                const date = msg.createdAt.toLocaleDateString();
+                const sizeBefore = messages.size;
+
+                if ( !messagesOld.has(link) ) {
+                    messages.add(link);
+                }
+                if ( messages.size !== sizeBefore ) {
+                    dates2[link] = date;
+                }
+            }
+        }));
+    }
+
+    return lastMessageId;
+};
+
+const loadDiscordMessages = async () => {
+    const client = new Discord.Client({
+        intents: [
+            Discord.Intents.FLAGS.GUILD_MESSAGES,
+            Discord.Intents.FLAGS.DIRECT_MESSAGES,
+            Discord.Intents.FLAGS.GUILDS,
+            Discord.Intents.FLAGS.GUILD_MESSAGES,
+            Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+            Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING
+        ],
+        partials: [
+            'MESSAGE',
+            'CHANNEL',
+            'REACTION'
+        ]
+    });
+
+    await client.login(process.env.BOT_TOKEN);
+
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+
+    await guild.channels.fetch();
+
+    const channel = guild.channels.cache.get(process.env.CHANNEL_ID);
+    const lastId = await loadMessages(channel);
+
+    console.log(messages);
+    console.log(dates2);
+    console.log(lastId);
+    fs.writeFileSync('./links11.js', 'module.exports = ' + JSON.stringify([...messages], null, 2));
+    fs.writeFileSync('./dates222.js', 'module.exports = ' + JSON.stringify(dates2, null, 2));
+};
+
+(async () => {
+    try {
+        await loadDiscordMessages();
+        const browser = await puppeteer.launch({
+            'headless': false,
+            'args': ['--fast-start', '--disable-extensions', '--no-sandbox'],
+            'ignoreHTTPSErrors': true
+        });
+
+        page = await browser.newPage();
+        await authorization();
+
+        let i = 1;
+
+        for ( const gamePage of messages ) {
+            // if  (i === 15) break;
+            const currentData = data[gamePage];
+
+            if ( !data[gamePage] || data[gamePage] === null ) {
+                console.log(i, ': ', gamePage);
+                i++;
+
+                const result = await loadPage(gamePage);
+
+                if ( result ) {
+                    result.date = dates2[gamePage];
+                }
+                data[gamePage] = result;
+            }
+        }
+
+        const replacer = ( key, value ) => (typeof value === 'undefined'
+            ? null
+            : value);
+
+        fs.writeFileSync('./data/data5.js', 'module.exports = ' + JSON.stringify(data, replacer, 2));
+        await browser.close();
+
+    } catch ( e ) {
+        console.log(e);
+    }
+})();
---
+++ avalon/puppeteer2.js
@@ -0,0 +1,315 @@
+'use strict';
+
+require('dotenv').config();
+
+const fs = require('fs');
+const ChartJSImage = require('chart.js-image');
+const data = require('../data/data5');
+const blueRoles = ['servant', 'percival', 'merlin'];
+const redRoles = ['mordred', 'assassin', 'morgana', 'oberon', 'minion'];
+const allRoles = blueRoles.concat(redRoles);
+const merlinRoleName = 'merlin';
+const nameRoleCountWin = {};
+const roleNameCountWin = {};
+
+let textResults;
+
+const sampleFunctionToSearchGameLinksByAnyConditions = () => {
+    Object.entries(a).forEach(( el, y ) => {
+        let i;
+        let e = el[1];
+        e?.names?.forEach(( name, index ) => {
+            if ( name.includes('Кроша ') ) {
+                i = index;
+            }
+        });
+        if ( i !== undefined && e?.roles[i] === 'oberon' ) {
+            console.log(el[0]);
+        }
+    });
+};
+
+const getTextResults = async () => {
+    try {
+        const games = Object.values(data).filter(e => e !== null);
+        const totalGames = games.length;
+        const totalRedWin = games.filter(e => e.redWin).length;
+        const totalRedWinByMissions = games.filter(e => e.redWin && e.byMissions).length;
+        const totalRedWinByMerlinShot = games.filter(e => e.redWin && !e.byMissions).length;
+        const totalBlueWin = games.filter(e => e.blueWin).length;
+
+        textResults = {
+            'Всего игр': totalGames,
+            'Побед синих: ': (totalBlueWin / totalGames * 100).toFixed(1) + '%',
+            'Побед красных: ': (totalRedWin / totalGames * 100).toFixed(1) + '%',
+            'Побед красных по походам: ': (totalRedWinByMerlinShot / totalRedWin * 100).toFixed(1) + '%',
+            'Побед красных по выстрелу в Мерлина: ': (totalRedWinByMissions / totalRedWin * 100).toFixed(1) + '%'
+        };
+        console.log(JSON.stringify(textResults, null, 2));
+    } catch ( e ) {
+        console.log(e);
+    }
+};
+
+const getBarCharts = async () => {
+
+};
+
+const getNameRoleCountWin = () => {
+    const games = Object.values(data).filter(e => e !== null);
+
+    for ( const game of games ) {
+        for ( let i = 0; i < game.names.length; i++ ) {
+            const name = game.names[i].trim();
+            const role = game.roles[i];
+            const hasWin = game.blueWin && blueRoles.includes(role) || !game.blueWin && !blueRoles.includes(role);
+            const isLiveMerlin = role === merlinRoleName && hasWin && !game.merlinShot
+                ? true
+                : undefined;
+            const isDeadMerlin = role === merlinRoleName && !hasWin && game.merlinShot
+                ? true
+                : undefined;
+
+            if ( !nameRoleCountWin[name] ) {
+                nameRoleCountWin[name] = {};
+            }
+            if ( !nameRoleCountWin[name][role] ) {
+                nameRoleCountWin[name][role] = {
+                    totalCount: 0,
+                    wins: 0,
+                    loses: 0
+                };
+                if ( role === merlinRoleName ) {
+                    nameRoleCountWin[name][role].liveMerlin = 0;
+                    nameRoleCountWin[name][role].deadMerlin = 0;
+                }
+            }
+            nameRoleCountWin[name][role].totalCount++;
+            if ( hasWin ) {
+                nameRoleCountWin[name][role].wins++;
+            } else {
+                nameRoleCountWin[name][role].loses++;
+            }
+            if ( isLiveMerlin ) {
+                nameRoleCountWin[name][role].liveMerlin++;
+            } else if ( isDeadMerlin ) {
+                nameRoleCountWin[name][role].deadMerlin++;
+            }
+        }
+    }
+};
+
+const getRoleNameCountWin = () => {
+    for ( const roleName of allRoles ) {
+        roleNameCountWin[roleName] = {};
+        for ( const name of Object.keys(nameRoleCountWin) ) {
+            if ( nameRoleCountWin[name].hasOwnProperty(roleName) ) {
+                const roleData = nameRoleCountWin[name][roleName];
+
+                if ( roleData.totalCount > 2 ) {
+                    const percent = (roleData.wins / roleData.totalCount * 100).toFixed(1);
+
+                    roleNameCountWin[roleName][name] = [];
+                    roleNameCountWin[roleName][name][0] = percent;
+                    roleNameCountWin[roleName][name][1] = `${percent}% из ${roleData.totalCount} игр`;
+                }
+            }
+        }
+    }
+};
+
+const run = async () => {
+    /*const a = ChartJSImage().chart({
+        'type': 'bar',
+        'data': {
+            'labels': [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July'
+            ],
+            'datasets': [
+                {
+                    'label': 'My First dataset',
+                    'borderColor': 'rgb(255,+99,+132)',
+                    'backgroundColor': 'rgba(255,+99,+132,+.5)',
+                    'data': [
+                        57,
+                        90,
+                        11,
+                        -15,
+                        37,
+                        -37,
+                        -27
+                    ]
+                },
+                {
+                    'label': 'My Second dataset',
+                    'borderColor': 'rgb(54,+162,+235)',
+                    'backgroundColor': 'rgba(54,+162,+235,+.5)',
+                    'data': [
+                        71,
+                        -36,
+                        -94,
+                        78,
+                        98,
+                        65,
+                        -61
+                    ]
+                },
+                {
+                    'label': 'My Third dataset',
+                    'borderColor': 'rgb(75,+192,+192)',
+                    'backgroundColor': 'rgba(75,+192,+192,+.5)',
+                    'data': [
+                        48,
+                        -64,
+                        -61,
+                        98,
+                        0,
+                        -39,
+                        -70
+                    ]
+                },
+                {
+                    'label': 'My Fourth dataset',
+                    'borderColor': 'rgb(255,+205,+86)',
+                    'backgroundColor': 'rgba(255,+205,+86,+.5)',
+                    'data': [
+                        -58,
+                        88,
+                        29,
+                        44,
+                        3,
+                        78,
+                        -9
+                    ]
+                }
+            ]
+        },
+        'options': {
+            'title': {
+                'display': true,
+                'text': 'Chart.js Line Chart'
+            },
+            'scales': {
+                'xAxes': [
+                    {
+                        'scaleLabel': {
+                            'display': true,
+                            'labelString': 'Month'
+                        }
+                    }
+                ],
+                'yAxes': [
+                    {
+                        'stacked': true,
+                        'scaleLabel': {
+                            'display': true,
+                            'labelString': 'Value'
+                        }
+                    }
+                ]
+            }
+        }
+    }) // Line chart
+        .backgroundColor('white').width(500) // 500px
+        .height(300); // 300px*/
+    // for (const roleName of allRoles) {
+    // const result = {};
+    // for (const role of nameRoleCountWin[roleName]) {
+    //
+    // }
+    // results.push(result);
+    // console.log(result);
+    // }
+
+    for ( const role of Object.keys(roleNameCountWin) ) {
+        // const role = Object.keys(roleNameCountWin)[0];
+
+        const roleNameCountWinSorted = Object
+            .entries(roleNameCountWin[role])
+            .sort(( a, b ) => b[1][0] - a[1][0])
+            .reduce(( _sortedObj, [k, v] ) => ({
+                ..._sortedObj,
+                [k]: v[1]
+            }), {});
+
+        const labels = Object.keys(roleNameCountWinSorted);
+        // let datasets = [];
+        // for (const name of Object.keys(roleNameCountWin[role])) {
+        //     datasets.push({
+        //         labels: name,
+        //         data: [roleNameCountWin[role][name]]
+        //     });
+        // }
+        const datasets = [
+            {
+                label: role,
+                data: Object.values(roleNameCountWinSorted)
+            }
+        ];
+
+        // console.log(labels);
+        // console.log(datasets[0].data);
+        const result = JSON.stringify(roleNameCountWinSorted, null, 4)
+            .replace(/\"/g, '\'')
+            .replace(/(\n    )'(.*)\s*'(:)/g, '$1$2$3');
+
+        console.log('\n', (role.charAt(0).toUpperCase() + role.slice(1)), '\n');
+        console.log(result);
+        /*let b = ChartJSImage().chart({
+            type: 'bar',
+            data: {
+                labels,
+                datasets
+            },
+            options: {
+                maintainAspectRatio: true,
+                spanGaps: false,
+                elements: {
+                    line: {
+                        tension: 0.000001
+                    }
+                },
+                title: {
+                    display: true,
+                    text: role
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            scaleLabel: {
+                                display: false,
+                                labelString: "Names"
+                            }
+                        }
+                    ],
+                    yAxes: [
+                        {
+                            stacked: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Percent"
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+            .backgroundColor('white')
+            .width(1900) // 500px
+            .height(1080);
+
+        await b.toFile('./roles2/' + role + '.png');*/
+        // await b.toFile('./chart9.png');
+    }
+};
+
+getTextResults();
+getNameRoleCountWin();
+getRoleNameCountWin();
+run();
