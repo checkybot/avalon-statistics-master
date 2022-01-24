d01c9bee6ec4025c1f78b80ebf6bacfa8d40000a
checkybot <checkybot@yandex.ru>
2022-01-24 03:22:26 +0200

    change year logic

--- avalon/puppeteer.js
+++ avalon/puppeteer.js
@@ -140,16 +140,16 @@ const loadMessages = async ( channel, lastMessageId ) => {
         lastMessageId = fetched.last().id;
         await Promise.all(fetched.map(async msg => {
             const isTooManyMessages = messages.size >= maximumMessageCount;
-            const isMonthCorrect = new Date(msg.createdTimestamp).getMonth() >= 0;
+            // const isMonthCorrect = new Date(msg.createdTimestamp).getMonth() >= 0;
             const isYearCorrect = new Date(msg.createdTimestamp).getFullYear() === 2022;
 
-            if ( isTooManyMessages ) {
+            if ( isTooManyMessages || isYearCorrect ) {
                 fetchComplete = true;
 
                 return;
             }
 
-            if ( isMonthCorrect && isYearCorrect && msg.cleanContent.includes('https://avalon.fun/GAME-') ) {
+            if ( msg.cleanContent.includes('https://avalon.fun/GAME-') ) {
                 const link = msg.cleanContent.match(/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,?^=%&:\/~+#-]*[\w?^=%&\/~+#-])/)[0];
                 const date = msg.createdAt.toLocaleDateString();
                 const sizeBefore = messages.size;
